cc06a83f9ab3744b771fc1b4167148cf149a2ceb
angcm25 <merari.perdomo@catolica.edu.sv>
2023-02-25 11:44:22 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/EquiposController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DAWpractica1.Models;
+
+namespace DAWpractica1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquiposController : ControllerBase
+    {
+        private readonly equipos_context _equipos_context;
+
+        public EquiposController(equipos_context equipos_context)
+        {
+            _equipos_context = equipos_context;
+        }
+
+        [HttpGet]
+        [Route("GetTodo")]
+        public IActionResult Get()
+        {
+            List<Equipos> listadoEquipo =(from e in _equipos_context.equipos
+                                          select e).ToList();
+            if (listadoEquipo.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoEquipo);
+        }
+    }
+
+}
---
+++ Models/equipos_context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DAWpractica1.Models
+{
+    public class equipos_context : DbContext
+    {
+        public equipos_context(DbContextOptions<equipos_context> options) : base(options) {
+            
+        }
+
+        public DbSet<Equipos> equipos { get; set; }
+    }
+}
